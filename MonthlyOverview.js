50eda8bb5aaaa5ef6730008864006cd314e4000a
paki <pavlovicnikola511@gmail.com>
2020-11-23 16:06:43 +0100

    collecting data...

--- MonthlyOverview.js
+++ MonthlyOverview.js
@@ -7,13 +7,13 @@ $('#month').append(currentMonth);
 
 const types = document.getElementById('types');
 
-let doneByMonth = [];
+let doneByMonth = []
 let workouts = JSON.parse(localStorage.getItem('workouts')) || [];
-console.log(workouts)
 workouts.map(workout => {
     // Filter done workouts by month
     if(currentMonth.includes(workout.date.split(' ')[1]) && workout.exercises){
         workout.exercises.map(a => doneByMonth.push(a));
+        doneByMonth.map(a => a['date'] = workout.date)
     }
 });
 
@@ -23,48 +23,44 @@ console.log(doneByMonth)
 var counts = [];
 let unique = [...new Set(doneByMonth.map(a => a.Name))]
 unique.map(i => counts.push({"name": i, "reps": []}))
-counts.map(i => doneByMonth.map(a => a.Name === i.name && i.reps.push(parseInt(a.Reps))))
+counts.map(i => doneByMonth.map(a => a.Name === i.name && i.reps.push({"num": parseInt(a.Reps), "date": a.date})))
+console.log(counts)
 counts.map(single => {
     types.innerHTML += `
-        <div class='workout-log' id='asd'>
-            <p>${single.name} ${single.reps.reduce((a, b) => a+b, 0)} [${single.reps.map(a => a)}]</p>
+        <div class='workout-log'>
+            <h3>${single.name} - ${single.reps.reduce((a, b) => a+b.num, 0)}</h3>
+            ${single.reps.map(a => `<p>${a.date.substr(0, 6)} - ${a.num}</p>`).join('')}
         </div>
     `
 });
 
-const asd = document.querySelectorAll('#asd');
-asd.map
-
 const a = getDaysInMonth(d.getMonth()+1, currentYear)
+const h = d.getDate();
 let days = [];
-for(let i = 1; i < a+1; i++) days.push(i);
+for(let i = h; i < a+1; i++) days.push(i);
 let reps = []
-counts.map(count => count.reps.map(rep => reps.push(rep)))
+counts.map(count => count.reps.map(rep => reps.push(rep.num)));
 
-new Chart(document.getElementById('myChart').getContext('2d'), {
+var ctx = document.getElementById('myChart').getContext('2d');
+var myChart = new Chart(ctx, {
     type: 'line',
     data: {
         labels: days,
         datasets: [{
-            label: 'Pushups',
-            backgroundColor: 'rgba(136, 0, 2, .4)',
-            borderColor: 'rgb(136, 0, 2)',
-            data: reps
+            label: 'Pushes',
+            data: reps,
+            backgroundColor: ['rgba(255, 99, 132, 0.2)'],
+            borderColor: ['rgba(255, 99, 132, 1)'],
+            borderWidth: 2
         }]
     },
-    options: {}
-});
-
-new Chart(document.getElementById('myChart').getContext('2d'), {
-    type: 'line',
-    data: {
-        labels: days,
-        datasets: [{
-            label: 'Pushups',
-            backgroundColor: 'rgba(136, 0, 2, .4)',
-            borderColor: 'rgb(136, 0, 2)',
-            data: reps
-        }]
-    },
-    options: {}
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
 });
